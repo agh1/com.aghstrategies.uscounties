78d96c95cad1a3a341d5d18cefa394d62a566004
Andrew Hunt <andrew@aghstrategies.com>
2016-02-11 17:47:54 -0500

    Handle states with names

--- uscounties.php
+++ uscounties.php
@@ -3251,7 +3251,7 @@ function uscounties_listcounties() {
     ),
   );
 
-  return $counties;
+  return array($countryIso => $counties);
 }
 
 /**
@@ -3260,40 +3260,74 @@ function uscounties_listcounties() {
 
 function uscounties_loadcounties() {
 
-  $counties = uscounties_listcounties();
+  $allCounties = uscounties_listcounties();
 
-  static $dao = NULL;
-  if (!$dao) {
-    $dao = new CRM_Core_DAO();
+  foreach ($allCounties as $countryIso => $counties) {
+    static $dao = NULL;
+    if (!$dao) {
+      $dao = new CRM_Core_DAO();
+    }
+
+    // Get array of states.
+    try {
+      $result = civicrm_api3('Country', 'getsingle', array(
+        'iso_code' => $countryIso,
+        'api.Address.getoptions' => array(
+          'field' => 'state_province_id',
+          'country_id' => '$value.id',
+          'sequential' => 0,
+        ),
+      ));
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      $error = $e->getMessage();
+      CRM_Core_Error::debug_log_message(ts('API Error: %1', array(
+        'domain' => 'com.aghstrategies.uscounties',
+        1 => $error,
+      )));
+      return FALSE;
+    }
+
+    if (empty($result['api.Address.getoptions']['values'])) {
+      return FALSE;
+    }
+    $states = $result['api.Address.getoptions']['values'];
+
+    // go state-by-state to check existing counties
+
+    foreach ($counties as $stateName => $state) {
+      $id = array_search($stateName, $states);
+      if ($id === FALSE) {
+        continue;
+      }
+
+      $check = "SELECT name FROM civicrm_county WHERE state_province_id = $id";
+      $results = CRM_Core_DAO::executeQuery($check);
+      $existing = array();
+      while ($results->fetch()) {
+        $existing[] = $results->name;
+      }
+
+      // identify counties needing to be loaded
+      $add = array_diff($state, $existing);
+
+      $insert = array();
+      foreach ($add as $county) {
+        $countye = $dao->escape($county);
+        $insert[] = "('$countye', $id)";
+      }
+
+      // put it into queries of 50 counties each
+      for($i = 0; $i < count($insert); $i = $i+50) {
+        $inserts = array_slice($insert, $i, 50);
+        $query = "INSERT INTO civicrm_county (name, state_province_id) VALUES ";
+        $query .= implode(', ', $inserts);
+        CRM_Core_DAO::executeQuery($query);
+      }
+    }
   }
 
-  // go state-by-state to check existing counties
-
-  foreach ($counties as $id => $state) {
-    $check = "SELECT name FROM civicrm_county WHERE state_province_id = $id";
-    $results = CRM_Core_DAO::executeQuery($check);
-    $existing = array();
-    while ($results->fetch()) {
-      $existing[] = $results->name;
-    }
-
-    // identify counties needing to be loaded
-    $add = array_diff($state, $existing);
-
-    $insert = array();
-    foreach ($add as $county) {
-      $countye = $dao->escape($county);
-      $insert[] = "('$countye', $id)";
-    }
-
-    // put it into queries of 50 counties each
-    for($i = 0; $i < count($insert); $i = $i+50) {
-      $inserts = array_slice($insert, $i, 50);
-      $query = "INSERT INTO civicrm_county (name, state_province_id) VALUES ";
-      $query .= implode(', ', $inserts);
-      CRM_Core_DAO::executeQuery($query);
-    }
-  }
+  return TRUE;
 }
 
 /**
