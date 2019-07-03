bc170fdc88852e734ccfe68995d8ed6e3bb002ae
Andrew Hunt <andrew@aghstrategies.com>
2019-07-03 09:17:51 -0400

    Fixes to make linter happy

--- uscounties.php
+++ uscounties.php
@@ -3,7 +3,6 @@
 /**
  * Return a list of all the counties
  */
-
 function uscounties_listcounties() {
   $countryIso = 'US';
   $counties = array(
@@ -3257,7 +3256,6 @@ function uscounties_listcounties() {
 /**
  * Check and load counties
  */
-
 function uscounties_loadcounties() {
 
   $allCounties = uscounties_listcounties();
@@ -3318,7 +3316,7 @@ function uscounties_loadcounties() {
       }
 
       // put it into queries of 50 counties each
-      for($i = 0; $i < count($insert); $i = $i+50) {
+      for ($i = 0; $i < count($insert); $i = $i + 50) {
         $inserts = array_slice($insert, $i, 50);
         $query = "INSERT INTO civicrm_county (name, state_province_id) VALUES ";
         $query .= implode(', ', $inserts);
@@ -3331,7 +3329,7 @@ function uscounties_loadcounties() {
 }
 
 /**
- * Implementation of hook_civicrm_install
+ * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
@@ -3340,7 +3338,7 @@ function uscounties_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_enable
+ * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
@@ -3349,13 +3347,7 @@ function uscounties_civicrm_enable() {
 }
 
 /**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
+ * Implements hook_civicrm_upgrade().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
  */
